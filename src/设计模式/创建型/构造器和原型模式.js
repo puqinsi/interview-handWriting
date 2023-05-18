61011199c0c66938999eff5aee3f7515a4e10100
ZhaoCe <17631807110@163.com>
2023-05-18 18:37:13 +0800

    feat: 增加设计模式

---
+++ src/设计模式/创建型/构造器和原型模式.js
@@ -0,0 +1,36 @@
+/* 构造器模式 */
+function Person1(name, age) {
+  this.name = name;
+  this.age = age;
+  this.sayName = () => console.log(`my name is ${this.name}`);
+}
+
+const a = new Person1("a", 18);
+a.sayName();
+
+/* 原型模式 */
+function Person2() {}
+
+Person2.prototype.name = "b";
+Person2.prototype.age = 18;
+Person2.prototype.sayName = function () {
+  console.log(`my name is ${this.name}`);
+};
+
+const b = new Person2();
+b.sayName();
+
+const c = new Person2();
+c.sayName();
+
+/* 混合模式 */
+function Person3(name, age) {
+  this.name = name;
+  this.age = age;
+}
+Person3.prototype.sayName = function () {
+  console.log(`my name is ${this.name}`);
+};
+
+const d = new Person3("d", 18);
+d.sayName();
